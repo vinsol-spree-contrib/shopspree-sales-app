f5d2cc3760450c505e65122afaf0a77604d785a4
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-05 13:34:25 +0530

    Modify _main_menu

---
+++ app/serializers/spree/product_serializer.rb
@@ -0,0 +1,8 @@
+module Spree
+  class ProductSerializer < ActiveModel::Serializer
+    attributes :id, :name, :description, :price, :display_price, :slug
+    has_many :product_properties, serializer: ProductPropertiesSerializer
+    has_many :images, serializer: ImageSerializer
+    has_many :variants_including_master, root: :variants, serializer: VariantSerializer
+  end
+end
