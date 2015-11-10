599b1f9962eb84c10faeef01d47ebb97460bc2c5
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-10 12:29:10 +0530

    Modify home_serializer.rb

--- app/serializers/spree/home_serializer.rb
+++ app/serializers/spree/home_serializer.rb
@@ -5,6 +5,6 @@ module Spree
     has_many :promo_offer_banners,    serializer: Spree::BannerSerializer
     has_many :brand_offer_banners,    serializer: Spree::BannerSerializer
     has_many :category_offer_banners, serializer: Spree::BannerSerializer
-    has_many :new_Arrival_banners,    serializer: Spree::BannerSerializer
+    has_many :new_arrival_banners,    serializer: Spree::BannerSerializer
   end
 end
