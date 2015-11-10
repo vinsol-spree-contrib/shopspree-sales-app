8f876d038e1f0c9be02c619ac7a29c0175854e6d
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-10 14:32:43 +0530

    Add association in ProductPropertyType

--- app/models/spree/product_property_type.rb
+++ app/models/spree/product_property_type.rb
@@ -1,5 +1,7 @@
 module Spree
   class ProductPropertyType < ActiveRecord::Base
+    has_many :product_properties, foreign_key: 'type_id', dependent: :restrict_with_error
+
     validates :name, presence: true
     validates :name, uniqueness: true, allow_blank: true
   end
