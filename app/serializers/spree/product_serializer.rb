e53498afb6a8a97a10b964bc22a24174a6574e80
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-13 17:48:59 +0530

    Modify product_serialier to add few fields, and override product_property_serializer to add type_name in it

--- app/serializers/spree/product_serializer.rb
+++ app/serializers/spree/product_serializer.rb
@@ -1,8 +1,23 @@
 module Spree
   class ProductSerializer < ActiveModel::Serializer
-    attributes :id, :name, :description, :price, :display_price, :slug
-    has_many :product_properties, serializer: ProductPropertiesSerializer
+    attributes :id, :name, :description, :price, :display_price, :slug, :ratings_count, :average_rating, :reviews_count
+
+    has_many :product_properties, serializer: ProductPropertySerializer
     has_many :images, serializer: ImageSerializer
     has_many :variants_including_master, root: :variants, serializer: VariantSerializer
+
+    ## Remove this fields when they are actually added to the product.
+
+    def ratings_count
+      40
+    end
+
+    def average_rating
+      3.4
+    end
+
+    def reviews_count
+      20
+    end
   end
 end
