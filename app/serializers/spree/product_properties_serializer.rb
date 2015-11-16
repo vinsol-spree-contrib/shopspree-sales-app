040fa432b0da43be08624b7e0a1600c68fd6ee13
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-16 11:10:19 +0530

    Modify product_properties_serializer and product_serializer

--- app/serializers/spree/product_properties_serializer.rb
+++ app/serializers/spree/product_properties_serializer.rb
@@ -1,5 +1,5 @@
 module Spree
-  class ProductPropertySerializer < ActiveModel::Serializer
+  class ProductPropertiesSerializer < ActiveModel::Serializer
     attributes :id, :product_id, :value, :property_name, :presentation, :type_name
 
     def type_name
--- app/serializers/spree/product_serializer.rb
+++ app/serializers/spree/product_serializer.rb
@@ -2,7 +2,7 @@ module Spree
   class ProductSerializer < ActiveModel::Serializer
     attributes :id, :name, :description, :price, :display_price, :slug, :ratings_count, :average_rating, :reviews_count
 
-    has_many :product_properties, serializer: ProductPropertySerializer
+    has_many :product_properties, serializer: ProductPropertiesSerializer
     has_many :images, serializer: ImageSerializer
     has_many :variants_including_master, root: :variants, serializer: VariantSerializer
 
