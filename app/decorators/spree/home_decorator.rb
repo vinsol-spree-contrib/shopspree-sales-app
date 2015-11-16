d6e5969e30cb62f55ff22de222bbce16ca152de2
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-16 14:36:38 +0530

    Remove (type)_banners methods and add banners method in home_serializer, home_decorator, add type in banner_serializer

--- app/decorators/spree/home_decorator.rb
+++ app/decorators/spree/home_decorator.rb
@@ -2,20 +2,9 @@ module Spree
   class HomeDecorator
     include ActiveModel::Serialization
 
-    def promo_offer_banners
-      Spree::PromoOfferBanner.all
+    def banners
+      Spree::Banner.all
     end
 
-    def brand_offer_banners
-      Spree::BrandOfferBanner.all
-    end
-
-    def category_offer_banners
-      Spree::CategoryOfferBanner.all
-    end
-
-    def new_arrival_banners
-      Spree::NewArrivalBanner.all
-    end
   end
 end
--- app/serializers/spree/banner_serializer.rb
+++ app/serializers/spree/banner_serializer.rb
@@ -2,10 +2,15 @@ module Spree
   class BannerSerializer < ActiveModel::Serializer
     attributes :id,
                :image_url,
-               :target_url
+               :target_url,
+               :type
 
     def image_url
       object.image.url(:thumb_960_540)
+    end
+
+    def type
+      object.type.demodulize
     end
   end
 end
