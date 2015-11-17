d81abb5c0193b8724fdccecfdffd6eed95aa00a5
nishant-cyro <nishant.tuteja@vinsol.com>
2015-11-17 12:26:56 +0530

    Modify add_spree_banner_types.rb

--- adhoc-scripts/add_spree_banner_types.rb
+++ adhoc-scripts/add_spree_banner_types.rb
@@ -4,5 +4,5 @@ banner_hash = {}
 
 ['promo_offer_banner', 'brand_offer_banner', 'new_arrival_banner', 'category_offer_banner'].each do |banner_name|
   banner_type = Spree::BannerType.where(name: banner_name).first_or_create(presentation: banner_name.humanize)
-  Spree::Banner.where(type: "Spree::#{ banner_name.classify }").update_all(type: banner_type.id)
+  Spree::Banner.where('type = ?', "Spree::#{ banner_name.classify }").update_all(type_id: banner_type.id)
 end
