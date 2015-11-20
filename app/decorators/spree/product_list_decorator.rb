b789d1391e1a5e8e3312034befa5005692e82c68
dilpreet92 <dilpreet@vinsol.com>
2015-11-20 12:54:37 +0530

    remove unused methods in product list serializer

--- app/decorators/spree/product_list_decorator.rb
+++ app/decorators/spree/product_list_decorator.rb
@@ -10,16 +10,8 @@ module Spree
 
     attr_reader :products, :product_scope, :prices
 
-    def categories
-      taxons_with_taxonomy_name('Categories')
-    end
-
     def filters
       Spree::Filter.get(self)
-    end
-
-    def brands
-      taxons_with_taxonomy_name('Brand')
     end
 
     def option_values
@@ -41,13 +33,9 @@ module Spree
     private
 
       def max_and_min_prices
-        Spree::Price.where(currency: Spree::Config[:currency]).joins(variant: :product).merge(Spree::Product.all)
+        Spree::Price.where(currency: Spree::Config[:currency]).joins(variant: :product).merge(product_scope)
                     .pluck('MAX(`spree_prices`.`amount`), MIN(`spree_prices`.`amount`)').flatten
       end
 
-      def taxons_with_taxonomy_name(taxonomy_name)
-        Spree::Taxon.joins(:taxonomy).merge(Spree::Taxonomy.where(name: taxonomy_name)).joins(classifications: :product)
-                  .merge(product_scope).includes(:children).uniq
-      end
   end
 end
--- app/serializers/spree/product_list_serializer.rb
+++ app/serializers/spree/product_list_serializer.rb
@@ -1,24 +1,8 @@
 module Spree
   class ProductListSerializer < ActiveModel::Serializer
-    attributes :maximum_price, :product_properties, :option_values, :minimum_price
 
-    has_many :categories, serializer: Spree::ProductList::TaxonSerializer
-    has_many :brands, serializer: Spree::ProductList::TaxonSerializer
     has_many :products, serializer: Spree::ProductList::ProductSerializer
     has_many :filters, serializer: Spree::ProductList::FilterSerializer
 
-    def product_properties
-      object.product_properties.inject(Hash.new { |h, k| h[k] = [] }) do |product_properties_hash, product_property|
-        product_properties_hash[product_property.property.presentation] << product_property.value
-        product_properties_hash
-      end
-    end
-
-    def option_values
-      object.option_values.inject(Hash.new { |h, k| h[k] = [] }) do |option_values_hash, option_value|
-        option_values_hash[option_value.option_type.presentation] << option_value.presentation
-        option_values_hash
-      end
-    end
   end
 end
