b69e028f2010087ecae5b9da1c793b012eb60c6f
dilpreet92 <dilpreet@vinsol.com>
2015-11-27 20:45:57 +0530

    Added user module to confirmations controller

--- app/controllers/spree/api/ams/user/confirmations_controller.rb
+++ app/controllers/spree/api/ams/user/confirmations_controller.rb
@@ -1,28 +1,30 @@
 module Spree
   module Api
     module Ams
-      class User::ConfirmationsController < Spree::Api::BaseController
-        include Serializable
-        include Requestable
+      module User
+        class ConfirmationsController < Spree::Api::BaseController
+          include Serializable
+          include Requestable
 
-        before_action :load_ams_user, only: :create
+          before_action :load_ams_user, only: :create
 
-        def create
-          if @user.send_confirmation_instructions
-            render json: { message: 'Mail successfully sent' }
-          else
-            render json: { errors: 'Unable to send mail' }, status: 422
-          end
-        end
-
-        private
-
-          def load_ams_user
-            unless @user = Spree.user_class.find_by(spree_api_key: params[:token])
-              render json: { errors: 'User not found' }, status: 404
+          def create
+            if @user.send_confirmation_instructions
+              render json: { message: 'Mail successfully sent' }
+            else
+              render json: { errors: 'Unable to send mail' }, status: 422
             end
           end
 
+          private
+
+            def load_ams_user
+              unless @user = Spree.user_class.find_by(spree_api_key: params[:token])
+                render json: { errors: 'User not found' }, status: 404
+              end
+            end
+
+        end
       end
     end
   end
