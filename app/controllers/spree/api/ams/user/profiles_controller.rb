db711b1158f7bfcb58f7c67d264173c38718d643
dilpreet92 <dilpreet@vinsol.com>
2015-11-27 20:37:57 +0530

    Added user module in profiles controller

--- app/controllers/spree/api/ams/user/profiles_controller.rb
+++ app/controllers/spree/api/ams/user/profiles_controller.rb
@@ -1,33 +1,35 @@
 module Spree
   module Api
     module Ams
-      class User::ProfilesController < Spree::Api::BaseController
-        include Serializable
-        include Requestable
+      module User
+        class ProfilesController < Spree::Api::BaseController
+          include Serializable
+          include Requestable
 
-        before_action :load_ams_user, only: :update
+          before_action :load_ams_user, only: :update
 
-        def update
-          authorize! :update, @user
-          if @user.update_attributes(user_update_params)
-            render json: @user, serializer: Spree::UserSerializer
-          else
-            invalid_resource!(@user)
-          end
-        end
-
-        private
-
-          def load_ams_user
-            unless @user = Spree.user_class.find_by(spree_api_key: params[:token])
-              render json: { errors: 'User not found' }, status: 404
+          def update
+            authorize! :update, @user
+            if @user.update_attributes(user_update_params)
+              render json: @user, serializer: Spree::UserSerializer
+            else
+              invalid_resource!(@user)
             end
           end
 
-          def user_update_params
-            params.require(:user).permit(:full_name, :phone)
-          end
+          private
 
+            def load_ams_user
+              unless @user = Spree.user_class.find_by(spree_api_key: params[:token])
+                render json: { errors: 'User not found' }, status: 404
+              end
+            end
+
+            def user_update_params
+              params.require(:user).permit(:full_name, :phone)
+            end
+
+        end
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127060250) do
+ActiveRecord::Schema.define(version: 20151127144720) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
