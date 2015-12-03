08e28871a105cdeae333afb656193db033a9c1c7
dilpreet92 <dilpreet@vinsol.com>
2015-12-03 14:14:44 +0530

    Added images to product

--- app/serializers/spree/product_list/product_serializer.rb
+++ app/serializers/spree/product_list/product_serializer.rb
@@ -2,6 +2,8 @@ module Spree
   module ProductList
     class ProductSerializer < ActiveModel::Serializer
       attributes :id, :name, :description, :price, :display_price, :slug
+
+      has_many :images
     end
   end
 end
