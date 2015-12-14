3ae4870c4260d07dd625160f3438d67b2663bcaf
vinsol <vinsol@vinsols-MacBook-Pro.local>
2015-12-14 19:02:17 +0530

    Rename method custom_reviews to reviewed_ratings

--- app/serializers/spree/product_serializer.rb
+++ app/serializers/spree/product_serializer.rb
@@ -2,13 +2,13 @@ module Spree
   class ProductSerializer < ActiveModel::Serializer
     attributes :id, :name, :description, :price, :display_price, :slug, :avg_rating, :reviews_count
 
-    has_many :custom_reviews, serializer: ReviewSerializer, key: 'reviews'
+    has_many :reviewed_ratings, serializer: ReviewSerializer, key: 'reviews'
     has_many :product_properties, serializer: ProductPropertiesSerializer
     has_many :images, serializer: ImageSerializer
     has_many :variants_including_master, root: :variants, serializer: VariantSerializer
 
 
-    def custom_reviews
+    def reviewed_ratings
       object.reviews.approved.reviewed_ratings.most_recent(object.review_limit || 5)
     end
 
