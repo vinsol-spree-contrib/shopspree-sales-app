562550ad7277bc8bff54900552b3faa2b5747cdc
Nimish <nimish@vinsol.com>
2016-01-04 20:09:16 +0530

    hackishly remove delivery step for now

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,7 @@
 Spree::Order.class_eval do
+
+  Spree::Order.state_machine.after_transition to: :delivery, do: :transition_to_payment
+
   def restart_checkout_flow
     self.update_columns(state: 'cart', updated_at: Time.current)
   end
@@ -8,8 +11,15 @@ Spree::Order.class_eval do
     previous_state = cart_states.index(self.state) - 1
     if previous_state < 0
       self.restart_checkout_flow
+    elsif cart_states[previous_state] == 'delivery'
+      self.update_columns(state: cart_states[previous_state - 1], updated_at: Time.current)
     else
       self.update_columns(state: cart_states[previous_state], updated_at: Time.current)
     end
   end
+
+  def transition_to_payment
+    self.next!
+  end
+
 end
